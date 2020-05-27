cbeb99d5b8b4b21ea779165b12e83a08ece96f97
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-27 13:48:22 -0400

    api/projects: add tests for the encoding operations

--- src/api/projects/repository/files/create.rs
+++ src/api/projects/repository/files/create.rs
@@ -182,6 +182,34 @@ mod tests {
     }
 
     #[test]
+    fn encoding_is_binary_safe() {
+        let items = &[(Encoding::Text, false), (Encoding::Base64, true)];
+
+        for (i, s) in items {
+            assert_eq!(i.is_binary_safe(), *s);
+        }
+    }
+
+    #[test]
+    fn encoding_encode_text() {
+        let encoding = Encoding::Text;
+        assert_eq!(encoding.encode(Some("foo"), b"foo"), "foo");
+    }
+
+    #[test]
+    #[should_panic = "attempting to encode non-utf8 content using text!"]
+    fn encoding_encode_text_bad() {
+        let encoding = Encoding::Text;
+        encoding.encode(None, b"\xff");
+    }
+
+    #[test]
+    fn encoding_encode_base64() {
+        let encoding = Encoding::Base64;
+        assert_eq!(encoding.encode(None, b"foo"), "Zm9v");
+    }
+
+    #[test]
     fn encoding_as_str() {
         let items = &[(Encoding::Text, "text"), (Encoding::Base64, "base64")];
 
